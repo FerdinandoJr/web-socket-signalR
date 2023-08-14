df45b27c516ec93faa5e4d242803d8990a91be26
Ferdinando Jr <93595042+FerdinandoJr@users.noreply.github.com>
2023-08-14 11:42:41 -0300

    FEAT - Servidor e Cliente implementados

---
+++ web-socket-signalR/Hubs/WebSocketHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Web;
+using System;
+
+namespace web_socket_signalR.Hubs
+{
+    public class WebSocketHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            Console.WriteLine("SERVIDOR RECEBEU!");
+
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        }
+    }
+}
--- web-socket-signalR/Program.cs
+++ web-socket-signalR/Program.cs
@@ -1,25 +1,34 @@
+using web_socket_signalR.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
-app.UseHttpsRedirection();
+
+//app.MapControllers(); 
+
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapRazorPages();
+app.MapHub<WebSocketHub>("/chatHub");
+
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
